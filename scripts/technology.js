071b006e4cbb9550e6e3312141ac5f92ab604705
Dmitry1397 <dyshik8@gmail.com>
2022-09-08 09:48:17 +0200

    adding slider on technology page

--- scripts/technology.js
+++ scripts/technology.js
@@ -8,6 +8,28 @@ fetch('scripts/data.json')
     .then((response) => response.json())
     .then((json) => array = json.technology);
 
+
+const swiper = new Swiper('.swiper', {
+        // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+});
+
+swiper.on('slideChange', function () {
+
+    console.log(swiper.realIndex)
+
+    update(swiper.realIndex);
+
+    buttons.forEach((element, index) => {
+        if (index === swiper.realIndex) {
+            element.setAttribute('class', 'active');
+        } else {
+            element.removeAttribute('class', 'active');
+        }
+    })
+});
+
 function update(index) {    
     const name = document.querySelector('#name');
     const image = document.querySelector('#image');
